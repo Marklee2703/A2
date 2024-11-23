0f202d76b9a8ef15ec77f0743d866ed330b76975
Marklee2703 <littlemark2703@gmail.com>
2024-11-23 23:26:20 +1300

    final

--- A2/settings.py
+++ A2/settings.py
@@ -151,7 +151,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ALL_ORIGINS = True
 CORS_ALLOWED_CREDENTIALS = True
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:3009']
+CORS_ALLOWED_ORIGINS = ['http://localhost:51662']
 CORS_ALLOWED_ORIGIN_REGEXS = [
-    'http://localhost:3009',
+    'http://localhost:51662',
 ]
